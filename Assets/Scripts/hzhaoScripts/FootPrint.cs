fb47207829675120dd2b86835bfc0ad00a40397d
hzhao <u1952264@unimail.hud.ac.uk>
2021-01-13 10:31:58 +0000

    backup copy

--- Assets/Scripts/hzhaoScripts/FootPrint.cs
+++ Assets/Scripts/hzhaoScripts/FootPrint.cs
@@ -8,39 +8,53 @@ public class FootPrint : MonoBehaviour
     public Vector3 offset;
     public GameObject leftFoot;
     public GameObject rightFoot;
+    Vector3 location;
+    Vector3 direction;
+    Vector3 rayOffSet;
+    Vector3 currentVelocity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        location = GetComponent<Transform>().position;
+        direction = new Vector3(0, -1, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //do createFootPrint if player moving;
+        /*currentVelocity = GetComponent<Rigidbody>().velocity;
+        if (currentVelocity.magnitude != 0)
+        {
+            CreateFootPrint();
+        }*/
+        Debug.DrawRay(location, direction * 5, Color.red);
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            CreateFootPrint();
+        }
     }
 
-    public void CreateFootPrint(GameObject prefab) {
+    public void CreateFootPrint() {
         RaycastHit hit;
         //to be change varible
-        Vector3 location = new Vector3(0, 0, 0);
-        Vector3 direction = new Vector3(0, 0, 0);
+       
         //player direction;
         Quaternion rotation = Quaternion.identity;
         direction = direction.normalized;
         if(Physics.Raycast(location, direction, out hit))
         {
+            
             Vector3 printLocation = hit.point;
             if (leftPrint)
             {
                 printLocation = printLocation + offset;
-                Instantiate(prefab, printLocation, rotation);
+                Instantiate(leftFoot, printLocation, rotation);
                 leftPrint = false;
             }
             else if (!leftPrint)
             {
                 printLocation = printLocation - offset;
-                Instantiate(prefab, printLocation, rotation);
+                Instantiate(rightFoot, printLocation, rotation);
                 leftPrint = true;
             }
             
